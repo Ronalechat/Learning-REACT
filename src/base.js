8de03fafa3425c23693b1e0097294b219ab6831c
Pinky <dir.michaellin@gmail.com>
2017-01-19 14:47:41 +1100

    Add Firebase support, and live edit and update.

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDhwNzIuct-MhZxLrYfui0X31uushs744U",
+  authDomain: "catch-of-the-day-mzl.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-mzl.firebaseio.com",
+});
+
+export default base; 
